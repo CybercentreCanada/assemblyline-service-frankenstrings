0694e751c0cb066079293770ef337df843446282
Jason Martyn <jmartyn@gmail.com>
2017-10-31 11:02:34 -0400

    Typo

--- balbuzard/patterns.py
+++ balbuzard/patterns.py
@@ -185,7 +185,7 @@ class PatternMatch(object):
                            r'(?:ALLUSERPROFILE|APPDATA|commonappdata|CommonProgramFiles|HOMEPATH|LOCALAPPDATA|' \
                            r'ProgramData|ProgramFiles|PUBLIC|SystemDrive|SystemRoot|\\TEMP|USERPROFILE|' \
                            r'windir|system32|syswow64|\\user|/home|/usr/local|/usr/bin|/var/log)' \
-                           r'/etc/(?:shadow|group|passwd))|[%]?\\[-_A-Z0-9\.\\]{1,200}\b'
+                           r'/etc/(?:shadow|group|passwd))[%]?\\[-_A-Z0-9\.\\]{1,200}\b'
         self.pat_fileext = r'(?i)\b[a-z]?[:]?[-_A-Z0-9.\\]{0,200}\w\.' \
                            r'(?:7Z|BAT|BIN|CLASS|CMD|DAT|DOC|DOCX|DLL|EML|EXE|JAR|JPG|JS|JSE|LOG|MSI|PDF|PNG|PPT|PPTX' \
                            r'|RAR|RTF|SCR|SWF|SYS|[T]?BZ[2]?|TXT|TMP|VBE|VBS|XLS|XLSX|ZIP)\b'
